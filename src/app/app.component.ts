d9071fdba7f47ed0cf3b8ceb881cc89f8b7c30cc
Tim <tassmann@students.uni-mainz.de>
2022-05-24 21:24:38 +0200

    started slideshwo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'timassmann';
+export class AppComponent implements OnInit {
+  images = ['caffee-laptop.jpg', 'coding-laptop.jpg', 'laptop.jpg'];
+  currentImage = 0;
+  showImage = true;
+
+  ngOnInit() {
+    this.updateImage();
+  }
+
+  updateImage() {
+    setInterval(() => {
+      this.currentImage++;
+      this.currentImage = this.currentImage % this.images.length;
+      this.showImage = false;
+      setTimeout(() => {
+        this.showImage = true;
+      }, 5);
+    }, 8000);
+  }
 }

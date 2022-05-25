98a0d4bdf41f3c776201b116a64d1ded96aa4f3c
Tim <tassmann@students.uni-mainz.de>
2022-05-25 11:18:39 +0200

    data-protection

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { SlideshowComponent } from './slideshow/slideshow.component';
+import { ImprintComponent } from './imprint/imprint.component'
+import { DataProtectionComponent } from './data-protection/data-protection.component';
+
+const routes: Routes = [
+  { path: '', component: SlideshowComponent },
+  { path: 'imprint', component: ImprintComponent },
+  { path: 'data-protection', component: DataProtectionComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
